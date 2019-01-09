4e41e80cf526c2aa4a5f146606510b81144c3fc0
yanghaoran <1064137887@qq.com>
2019-01-09 15:00:58 +0800

    【简要描述】：初步创建Mesh类 【详细描述】： 	1.初步创建Mesh类 	2.Mesh类中存放模型的顶点和纹理信息，以及VAO，VBO，EBO 	3.傅老师OpenGL视频第三章Mesh(1) 【修 改 人】：杨浩然

---
+++ OpenGLExcercise01/Mesh.cpp
@@ -0,0 +1,24 @@
+
+
+#include "Mesh.h"
+
+Mesh::Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures)
+{
+
+}
+
+void Mesh::Draw(Shader *shader)
+{
+
+}
+
+void Mesh::SetupMesh()
+{
+
+}
+
+
+Mesh::~Mesh()
+{
+
+}
