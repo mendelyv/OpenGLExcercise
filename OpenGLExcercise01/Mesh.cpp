59ba615ba2dec998ab36200e3b4f0847b40cf9ad
yanghaoran <1064137887@qq.com>
2019-01-09 15:41:24 +0800

    【简要描述】：完成Mesh类 【详细描述】： 	1.完成Mesh类中的成员方法 	2.傅老师OpenGL视频第三章Mesh(3) 【修 改 人】：杨浩然

--- OpenGLExcercise01/Mesh.cpp
+++ OpenGLExcercise01/Mesh.cpp
@@ -1,20 +1,64 @@
-
+﻿
 
 #include "Mesh.h"
 
+#include <gl/glew.h>
+
 Mesh::Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures)
 {
+	this->vertices = vertices;
+	this->indices = indices;
+	this->textures = textures;
 
+	SetupMesh();
 }
 
 void Mesh::Draw(Shader *shader)
 {
+	for (unsigned int i = 0; i < textures.size(); i++)
+	{
+		if (textures[i].type == "texture_diffuse")
+		{
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, textures[i].id);
+			shader->SetUniform1i("material.diffuse", 0);
+		}
+		else if (textures[i].type == "texture_specular")
+		{
+			glActiveTexture(GL_TEXTURE1);
+			glBindTexture(GL_TEXTURE_2D, textures[i].id);
+			shader->SetUniform1i("material.specular", 1);
+		}
 
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+		glActiveTexture(GL_TEXTURE0);
+	}
 }
 
+//配置VAO，VBO，EBO
 void Mesh::SetupMesh()
 {
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
 
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertices.size(), &vertices[0], GL_STATIC_DRAW);
+
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indices.size(), &indices[0], GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)3);
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)6);
+
+	glBindVertexArray(0);//解绑
 }
 
 
